f4c1b865e049379e6c2b7d8ac34dc2aca2d86d96
LeonAck <leonackerman.b@gmail.com>
2025-05-07 10:48:32 +0200

    first commit

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
 def preprocess_data(df):
-    # Implement preprocessing steps such as handling missing values, encoding categorical variables, etc.
+
+    # Ensure time is datetime type
+    train_df['time'] = pd.to_datetime(train_df['time'])
+
+    # Convert Valencia_wind_deg to numerical values
+    df['Valencia_wind_deg'] = df['Valencia_wind_deg'].astype(str).str.replace('level_', '').astype(int)
+
+    # Convert Seville_pressure to numerical values
+    df['Seville_pressure'] = df['Seville_pressure'].astype(str).str.replace('sp', '').astype(int)
+
     return df
+
+
