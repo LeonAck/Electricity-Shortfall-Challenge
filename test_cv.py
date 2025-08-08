b6095365d682ca23bb7848c7481fa2b308acea09
LeonAck <leonackerman.b@gmail.com>
2025-08-08 08:55:45 +0200

    mlflow model registry working

--- test_cv.py
+++ test_cv.py
@@ -1,9 +1,8 @@
 from scripts.data_loading import load_data, test_training_data
 from scripts.config_and_logging import generate_run_id, save_run_metadata, create_output_dir, log_to_mlflow, save_model_and_pipeline, run_with_config, load_config_hydra
 from scripts.model_pipeline_cv2 import choose_best_model
+from sklearn.metrics import root_mean_squared_error, make_scorer
 
-import hydra
-from omegaconf import DictConfig, OmegaConf
 from pathlib import Path
 import logging
 
@@ -41,32 +40,36 @@ def main(config_name="config_cv.yaml"):
 if __name__ == "__main__":
     main()
 
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, TimeSeriesSplit
-from scripts.preprocessing import get_pipeline_for_model
-from scripts.models import get_model
-from scripts.cross_validation_and_tuning import create_rmse_scorer
-# Get the project root director
-config = load_config_hydra(config_name="config.yaml", config_path=str(config_path))
-    
 
-train_df, _, _ = load_data(config)
-model_config = config['models'][1]
-pipeline = get_pipeline_for_model(model_config, config)
-model = get_model(model_config["type"], model_config['params'])
+if False: 
+    from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, TimeSeriesSplit
+    from scripts.preprocessing import get_pipeline_for_model
+    from scripts.models import get_model
+    from sklearn.metrics import make_scorer
 
-y_train = train_df[config['data']['target_column']]
-X_train = train_df.drop(columns=[config['data']['target_column']])
-X_train_processed = pipeline.fit_transform(X_train)
+    # Get the project root director
+    config = load_config_hydra(config_name="config.yaml", config_path=str(config_path))
+        
 
-cv = TimeSeriesSplit(n_splits=5)
-rmse_scorer = create_rmse_scorer()
-search = GridSearchCV(
-        estimator=model,
-        param_grid={'alpha': [0.1, 0.5]},
-        cv=cv,
-        scoring=rmse_scorer,
-        n_jobs=1,
-        verbose=2
-    )
-search.fit(X_train_processed, y_train)
-print("Best score from GridSearchCV:", search.best_score_)
+    train_df, _, _ = load_data(config)
+    model_config = config['models'][1]
+    pipeline = get_pipeline_for_model(model_config, config)
+    model = get_model(model_config["type"], model_config['params'])
+
+    y_train = train_df[config['data']['target_column']]
+    X_train = train_df.drop(columns=[config['data']['target_column']])
+    X_train_processed = pipeline.fit_transform(X_train)
+
+    cv = TimeSeriesSplit(n_splits=5)
+    rmse_score = make_scorer(root_mean_squared_error, response_method='predict', greater_is_better=False)
+    search = GridSearchCV(
+            estimator=model,
+            param_grid={'alpha': [0.1, 0.5]},
+            cv=cv,
+            scoring=rmse_score,
+            n_jobs=1,
+            verbose=2
+        )
+    search.fit(X_train_processed, y_train)
+    print("Best score from GridSearchCV:", -search.best_score_)
+    print("Best parameters from GridSearchCV:", search.best_params_)
